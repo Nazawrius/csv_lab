d81d74d401187d0eb68a4e6fb5850e40cfd15b32
Nazawrius <nazar022030@gmail.com>
2021-05-12 19:06:39 +0300

    ini processing

--- main.py
+++ main.py
@@ -1,14 +1,38 @@
 from sys import argv
+import json
 
 def load_data(ini_path):
-    with open(ini_path, 'r') as ini:
-        for line in ini:
-            print(line, end='')
+    ini = json.load(open(ini_path))
+
+    if 'input' not in ini or 'output' not in 'ini':
+        raise Exception
+
+    input = ini['input']
+    if 'json' not in input or 'csv' not in input or 'encoding' not in input:
+        raise Exception
+
+    output = ini['output']
+    if 'fname' not in output or 'encoding' not in output:
+        raise Exception
+
+    return input, output
 
 def main():
     print('description')
+
     print('*****')
-    load_data(argv[1])
+
+    if len(argv) != 2:
+        print('*****program aborted*****')
+        return None
+
+    try:
+        input, output = load_data(argv[1])
+    except Exception:
+        print('*****program aborted*****')
+        return None
+    else:
+        print(f'ini {argv[1]}: OK')
 
 if __name__ == '__main__':
     main()
