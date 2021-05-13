eda39390f3825be58cab78881a634c30328d15a0
Nazawrius <46971970+Nazawrius@users.noreply.github.com>
2021-05-13 11:25:43 +0300

    Added Invoice.number_of_repeats

--- models.py
+++ models.py
@@ -5,6 +5,7 @@ class Invoice:
             raise Exception
 
         self.entries = dict()
+        self.number_of_repeats = dict()
         self.add_product(row[1:])
 
     def add_product(self, row):
@@ -12,7 +13,13 @@ class Invoice:
         if not number.isdigit() or number[0] == 0:
             raise Exception
         else:
-            self.entries[int(number)] = Entry(row[2:])
+            self.entries[int(number)] = Entry(row[1:])
+            
+        entry_name = self.entries[int(number)].name
+        if entry_name in self.number_of_repeats:
+            self.number_of_repeats[entry_name] += 1
+        else:
+            self.number_of_repeats[entry_name] = 1
 
     def __repr__(self): #Not finished!!!
         s = self.number + '\n'
